76865a8e23bb6347fcf13cdac9a88ce589fef54d
Tarik Alkasab <tarik@alkasab.org>
2024-04-07 14:00:51 -0400

    Also builds set and element list schemas.

--- build_schemas.py
+++ build_schemas.py
@@ -3,6 +3,8 @@
 
 import json
 
+JSON_SCHEMA_URL = "http://json-schema.org/draft-07/schema#"
+
 SET_SCHEMA_FILENAME = "cde.schema.json"
 SET_LIST_SCHEMA_FILENAME = "cde_set_list.schema.json"
 ELEMENT_SCHEMA_FILENAME = "cde_element.schema.json"
@@ -11,15 +13,69 @@ ELEMENT_LIST_SCHEMA_FILENAME = "cde_element_list.schema.json"
 with open(SET_SCHEMA_FILENAME, "r") as f:
     set_schema = json.load(f)
 
-COMMON_DEFINITIONS = ["version", "schema_version", "status", "index_code", "contributors", "event", "specialty", "reference", "integer_value", "value_set", "float_value", "boolean_value", "person", "organization", "value", "image"]
-ELEMENT_DEFINITIONS = ["element", *COMMON_DEFINITIONS]
+ELEMENT_DEFINITIONS = [
+    "element",
+    "version",
+    "schema_version",
+    "status",
+    "index_code",
+    "contributors",
+    "event",
+    "specialty",
+    "reference",
+    "integer_value",
+    "value_set",
+    "float_value",
+    "boolean_value",
+    "person",
+    "organization",
+    "value",
+    "image",
+]
 
-element_schema = {
-    "$schema": "http://json-schema.org/draft-07/schema#",
-    "$ref": "#/definitions/element",
-    "$id": set_schema["$id"].replace(SET_SCHEMA_FILENAME, ELEMENT_SCHEMA_FILENAME),
-    "definitions": { def_name: set_schema["definitions"][def_name] for def_name in ELEMENT_DEFINITIONS }
+element_definitions = {
+    def_name: set_schema["definitions"][def_name] for def_name in ELEMENT_DEFINITIONS
 }
 
-with open(ELEMENT_SCHEMA_FILENAME, "w") as f:
-    json.dump(element_schema, f, indent=4)
+element_schema = {
+    "$schema": JSON_SCHEMA_URL,
+    "$ref": "#/definitions/element",
+    "$id": set_schema["$id"].replace(SET_SCHEMA_FILENAME, ELEMENT_SCHEMA_FILENAME),
+    "definitions": element_definitions,
+}
+
+set_list_schema = {
+    "$schema": JSON_SCHEMA_URL,
+    "$ref": "#/definitions/set_list",
+    "$id": set_schema["$id"].replace(SET_SCHEMA_FILENAME, SET_LIST_SCHEMA_FILENAME),
+    "definitions": {
+        "set_list": {
+            "type": "array",
+            "items": {"$ref": "#/definitions/element_set"},
+        },
+        **set_schema["definitions"],
+    },
+}
+
+element_list_schema = {
+    "$schema": JSON_SCHEMA_URL,
+    "$ref": "#/definitions/element_list",
+    "$id": set_schema["$id"].replace(SET_SCHEMA_FILENAME, ELEMENT_LIST_SCHEMA_FILENAME),
+    "definitions": {
+        "element_list": {
+            "type": "array",
+            "items": {"$ref": "#/definitions/element"},
+        },
+        **element_definitions,
+    },
+}
+
+
+def write_schema_file(filename, schema):
+    with open(filename, "w") as f:
+        json.dump(schema, f, indent=4)
+
+
+write_schema_file(ELEMENT_SCHEMA_FILENAME, element_schema)
+write_schema_file(SET_LIST_SCHEMA_FILENAME, set_list_schema)
+write_schema_file(ELEMENT_LIST_SCHEMA_FILENAME, element_list_schema)
